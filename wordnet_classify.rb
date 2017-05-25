e9a5eb897e03b59d98f9f30dbdd9ee0fa6ccbfff
RINDO <rindo.katsura@gmail.com>
2017-05-25 16:37:48 +0900

    fix classify

--- wordnet_classify.rb
+++ wordnet_classify.rb
@@ -14,35 +14,40 @@ def xml
   @_xml ||= REXML::Document.new(open('resources/jpn-asynset.xml'))
 end
 
-# parse wordnet affect ------------------------------------------------------
+def parse_list(path)
+  xml.elements.each(path) do |noun_syn|
+    cat = noun_syn['categ']
+    noun_syn.elements.each('jpn-word') do |_|
+      word = _['lemma']
+      kana = Word::Kana.kana(word)
+      pos  = Word::Pos.convert(_['pos'])
 
-ok_rows = []
-ng_rows = []
-
-xml.elements.each('syn-list/noun-syn-list/noun-syn') do |noun_syn|
-
-  cat = noun_syn['categ']
-  noun_syn.elements.each('jpn-word') do |_|
-    word = _['lemma']
-    kana = Word::Kana.kana(word)
-    pos  = Word::Pos.convert(_['pos'])
-
-    if key = classifier.classify(cat)
-      ok_rows << [key, word, kana, pos, cat]
-    else
-      ng_rows << [cat, word, kana, pos]
+      if key = classifier.classify(cat)
+        @ok_rows << [key, word, kana, pos, cat]
+      else
+        @ng_rows << [cat, word, kana, pos]
+      end
     end
   end
 end
+
+# parse wordnet affect ------------------------------------------------------
+
+@ok_rows = []
+@ng_rows = []
+
+parse_list('syn-list/noun-syn-list/noun-syn')
+parse_list('syn-list/adj-syn-list/adj-syn')
+parse_list('syn-list/adv-syn-list/adv-syn')
 
 # save to csv ---------------------------------------------------------------
 
 CSV.open('dest/ok.csv', 'w') do |csv|
   csv << ['emotion', 'word', 'kana', 'pos', 'cat']
-  ok_rows.each{|_| csv << _}
+  @ok_rows.each{|_| csv << _}
 end
 
 CSV.open('dest/ng.csv', 'w') do |csv|
   csv << ['cat', 'word', 'kana', 'pos']
-  ng_rows.each{|_| csv << _}
+  @ng_rows.each{|_| csv << _}
 end
